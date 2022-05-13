7a7995789c983ab233f7ba5fda2f2ff56c44951d
Raffaelbdl <raffaelbdl@gmail.com>
2022-05-14 01:08:33 +0200

    :sparkles: Add video tools

---
+++ school_idol_training/video_tools.py
@@ -0,0 +1,29 @@
+import subprocess
+import time
+
+import cv2
+
+
+def play_video_with_sound(video_path: str) -> None:
+    """Uses ffmpeg"""
+    subprocess.run(f"ffplay {video_path} -autoexit", shell=True)
+
+
+def test_camera() -> int:
+    """Returns fps"""
+    cap = cv2.VideoCapture(0)
+
+    start = time.time()
+    n_frames = 0
+    while cap.isOpened():
+        n_frames += 1
+        ret, frame = cap.read()
+        cv2.imshow("Test Camera", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    total = time.time() - start
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return int(n_frames / total)
