bb5e1dac324a9f73ffe664138a0c2003ee416072
Raffaelbdl <raffaelbdl@gmail.com>
2022-05-20 16:24:41 +0200

    :hammer: Change for _src structure

--- sip/__init__.py
+++ sip/__init__.py
@@ -1,70 +1,14 @@
-from dataclasses import dataclass, field
-from typing import Dict, List
-
-from mediapipe.framework.formats.landmark_pb2 import NormalizedLandmarkList
-import numpy as np
-
-
-JOINT_PAIRS = [
-    ["right_elbow", "right_wrist"],
-    ["right_shoulder", "right_elbow"],
-    ["right_shoulder", "right_hip"],
-    ["right_hip", "right_knee"],
-    ["right_knee", "right_ankle"],
-    ["left_elbow", "left_wrist"],
-    ["left_shoulder", "left_elbow"],
-    ["left_shoulder", "left_hip"],
-    ["left_hip", "left_knee"],
-    ["left_knee", "left_ankle"],
-]
-
-LANDMARK_NAMES = [
-    "nose",
-    "left_eye_inner",
-    "left_eye",
-    "left_eye_outer",
-    "right_eye_inner",
-    "right_eye",
-    "right_eye_outer",
-    "left_ear",
-    "right_ear",
-    "mouth_left",
-    "mouth_right",
-    "left_shoulder",
-    "right_shoulder",
-    "left_elbow",
-    "right_elbow",
-    "left_wrist",
-    "right_wrist",
-    "left_pinky_1",
-    "right_pinky_1",
-    "left_index_1",
-    "right_index_1",
-    "left_thumb_2",
-    "right_thumb_2",
-    "left_hip",
-    "right_hip",
-    "left_knee",
-    "right_knee",
-    "left_ankle",
-    "right_ankle",
-    "left_heel",
-    "right_heel",
-    "left_foot_index",
-    "right_foot_index",
-]
-
-
-@dataclass
-class Choregraphy:
-    """A choregraphy is described by its title and its video"""
-
-    title: str
-    keypoints: List[Dict[str, List[float]]]
-    landmarks: List[NormalizedLandmarkList]
-
-    video_path: str = field(default=None)
-    baseline_keypoints: Dict[str, np.ndarray] = field(default=None)
-    baseline_score: float = field(default=None)
-
-    score: float = field(default=None)  # for trainee
+from sip._src.chore import Choregraphy
+from sip._src.chore import load_chore
+from sip._src.chore import make_chore_from_file
+from sip._src.chore import save_chore
+from sip._src.dance import dance_along
+from sip._src.keypoint import get_keypoints_from_video_file
+from sip._src.keypoint import keypoints_to_time_series
+from sip._src.metric import cosine
+from sip._src.score import cosine_similarity
+from sip._src.sequence import interpolate_time_series
+from sip._src.sequence import union_of_masks
+from sip._src.video import play_video_with_sound
+from sip._src.video import test_camera
+from sip._src.video import record_camera
